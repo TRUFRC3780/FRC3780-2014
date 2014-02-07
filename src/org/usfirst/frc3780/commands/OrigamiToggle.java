4519ffa015a4e136d695af3a05f8ded43b19ea02
AndrewTLee <andytaelee@gmail.com>
2014-02-07 12:44:47 -0500

    Added OrigamiToggle command

---
+++ src/org/usfirst/frc3780/commands/OrigamiToggle.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc3780.commands;
+
+import edu.wpi.first.wpilibj.*;
+
+// This command is a bit iffy compared with the other two
+public class OrigamiToggle extends CommandBase {
+	private boolean initialState = origami.isExtended();
+	
+	public void execute() {
+		origami.toggleAll();
+	}
+	
+	public boolean isFinished() {
+		return initialState != origami.isExtended();
+	}
+}
