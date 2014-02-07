e2cf98b340b48d8c61c3eac7d404838e06217bfd
AndrewTLee <andytaelee@gmail.com>
2014-02-07 11:02:01 -0500

    First draft of piston class and with custom piston exception

---
+++ src/org/usfirst/frc3780/components/Piston.java
@@ -0,0 +1,53 @@
+// Sorry guys this is andy I'm using one line comments because
+// I'm too lazy for /* */
+//
+// This is a class for representing pistons on the 3780 2014 robot
+// For use with the "origamibot" subsystem
+package org.usfirst.frc3780.components;
+
+import edu.wpi.first.wpilibj.*;
+
+public class Piston {
+	// one field, one double solenoid
+	private DoubleSolenoid solenoid;
+	
+	// public constructor takes same input as DoubleSolenoid Constructor
+	public Piston(int moduleNumber,int forwardChannel,int reverseChannel) {
+		this( new DoubleSolenoid(moduleNumber,forwardChannel,reverseChannel) );
+	}
+	
+	// private constructor takes the solenoid object directly and initializes the field
+	private Piston(DoubleSolenoid solenoid) {
+		this.solenoid = solenoid;
+	}
+	
+	// toggle extended and retracted
+	public void toggle() throws PistonException {
+		if( isExtended() ) {
+			retract();
+		} else {
+			extend();
+		}
+	}
+	
+	// extend the piston using the solenoid
+	public void extend() throws PistonException {
+		if( isExtended() ) throw new PistonException("Attempt to extend piston that is already extended: " + this);
+		solenoid.set( DoubleSolenoid.Value.kForward );
+	}
+	
+	// retract the piston using the solenoid
+	public void retract() throws PistonException {
+		if( !isExtended() ) throw new PistonException("Attempt to retract piston that is already retracted" + this);
+		solenoid.set( DoubleSolenoid.Value.kReverse );
+	}
+	
+	// return boolean of whether piston is extended
+	public boolean isExtended() throws PistonException {
+		DoubleSolenoid.Value state = solenoid.get();
+		if( state == DoubleSolenoid.Value.kForward ) return true;
+		if( state == DoubleSolenoid.Value.kReverse ) return false;
+		throw new PistonException("Piston is not in forward or reverse states");
+	}
+	
+}
