f85aeb7432d22de474bce1b91337a438bb118dfd
bfogarty <bri@nfogarty.me>
2014-04-07 09:21:52 -0400

    License.

--- src/org/usfirst/frc3780/components/Controller.java
+++ src/org/usfirst/frc3780/components/Controller.java
@@ -1,3 +1,19 @@
+/*
+* Copyright 2014 FRC3780 (B. Fogarty, E. Rothchild, A. Lee) Licensed under the
+* Educational Community License, Version 2.0 (the "License"); you may
+* not use this file except in compliance with the License. You may
+* obtain a copy of the License at
+* 
+* http://www.opensource.org/licenses/ECL-2.0
+* 
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an "AS IS"
+* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+* or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+* 
+*/
+
 package org.usfirst.frc3780.components;
 
 import edu.wpi.first.wpilibj.*;
--- src/org/usfirst/frc3780/components/Piston.java
+++ src/org/usfirst/frc3780/components/Piston.java
@@ -1,12 +1,27 @@
-// Sorry guys this is andy I'm using one line comments because
-// I'm too lazy for /* */
-//
-// This is a class for representing pistons on the 3780 2014 robot
-// For use with the "origamibot" subsystem
+/*
+* Copyright 2014 FRC3780 (B. Fogarty, E. Rothchild, A. Lee) Licensed under the
+* Educational Community License, Version 2.0 (the "License"); you may
+* not use this file except in compliance with the License. You may
+* obtain a copy of the License at
+* 
+* http://www.opensource.org/licenses/ECL-2.0
+* 
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an "AS IS"
+* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+* or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+* 
+*/
+
 package org.usfirst.frc3780.components;
 
 import edu.wpi.first.wpilibj.*;
 
+/**
+ * This is a class for representing pistons. For use with the Origami subsystem.
+ * @author Andy
+ */
 public class Piston {
 	// one field, one double solenoid
 	private DoubleSolenoid solenoid;
